145a0782ee2c9dba17568371662502c708465d4e
Shrinivas <shrinivas.bp@aerchain.io>
2024-05-31 21:31:39 +0530

    updated security

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import openAiService from './src/index.js';
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
